5ee14673cf8627eca0ca0661e7acdfaf8b70ec5f
Yasser Hussain <hussaiy1@gmail.com>
2025-01-07 22:38:39 +0100

    adding dockerfile

--- src/utils.ts
+++ src/utils.ts
@@ -5,25 +5,38 @@ import * as path from "path";
 import { fileURLToPath } from "url";
 
 export function startCountdown(remainingTime: number) {
-  const countdown = setInterval(() => {
-    remainingTime -= 1000;
-    if (remainingTime > 0) {
-      const hours = Math.floor(remainingTime / (60 * 60 * 1000));
-      const minutes = Math.floor(
-        (remainingTime % (60 * 60 * 1000)) / (60 * 1000)
-      );
-      const seconds = Math.floor((remainingTime % (60 * 1000)) / 1000);
-
-      process.stdout.clearLine(0);
-      process.stdout.cursorTo(0);
-      process.stdout.write(
-        `Time until next tweet: ${hours}h ${minutes}m ${seconds}s`
-      );
-    } else {
-      clearInterval(countdown);
-    }
-  }, 1000);
-}
+    const countdown = setInterval(() => {
+      remainingTime -= 1000;
+  
+      if (remainingTime > 0) {
+        const hours = Math.floor(remainingTime / (60 * 60 * 1000));
+        const minutes = Math.floor(
+          (remainingTime % (60 * 60 * 1000)) / (60 * 1000)
+        );
+        const seconds = Math.floor((remainingTime % (60 * 1000)) / 1000);
+  
+        if (process.stdout.isTTY) {
+          process.stdout.clearLine(0);
+          process.stdout.cursorTo(0);
+          process.stdout.write(
+            `Time until next tweet: ${hours}h ${minutes}m ${seconds}s`
+          );
+        } else {
+          console.log(
+            `Time until next tweet: ${hours}h ${minutes}m ${seconds}s`
+          );
+        }
+      } else {
+        clearInterval(countdown);
+  
+        if (process.stdout.isTTY) {
+          process.stdout.clearLine(0);
+          process.stdout.cursorTo(0);
+        }
+        console.log("Countdown complete!");
+      }
+    }, 1000);
+  }
 
 export function getEnvVariables(): Record<string, string | undefined> {
   return { ...process.env };
